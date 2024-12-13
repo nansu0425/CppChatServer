d6429985fa6f01645ff0a93ec6dcb97d90c6a275
NanSu <nansu0425@gmail.com>
2024-12-13 22:17:33 +0900

    Add project files.

---
+++ tests/Content.Tests/Content.Tests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ContentTests
+{
+	TEST_CLASS(ContentTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
