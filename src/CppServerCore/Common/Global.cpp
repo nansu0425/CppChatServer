7ceb88d45192f6f7c4d92f77ed2d53f0f8b0680d
NanSu <nansu0425@gmail.com>
2024-12-17 21:21:33 +0900

    Added g_pThreadManager

--- src/CppServerCore/Common/Global.cpp
+++ src/CppServerCore/Common/Global.cpp
@@ -1,3 +1,9 @@
 ﻿// CppServerCore/Common/Global.cpp
 
 #include <CppServerCore/Pch.hpp>
+#include <CppServerCore/Thread/ThreadManager.hpp>
+
+namespace CppServerCore::Common
+{
+    std::unique_ptr<Thread::ThreadManager>      g_pThreadManager = std::make_unique<Thread::ThreadManager>();
+}
--- src/CppServerCore/Common/Global.hpp
+++ src/CppServerCore/Common/Global.hpp
@@ -1,3 +1,13 @@
 ﻿// CppServerCore/Common/Global.hpp
 
 #pragma once
+
+namespace CppServerCore::Thread
+{
+    class ThreadManager;
+}
+
+namespace CppServerCore::Common
+{
+    extern std::unique_ptr<Thread::ThreadManager>     g_pThreadManager;
+}
