1be392590a4f4931bd6adf6c0db2056580777747
NanSu <nansu0425@gmail.com>
2024-12-17 18:30:44 +0900

    Added ThreadManager

---
+++ src/CppServerCore/Thread/ThreadManager.cpp
@@ -0,0 +1,55 @@
+﻿// CppServerCore/Thread/ThreadManager.cpp
+
+#include <CppServerCore/Pch.hpp>
+#include <CppServerCore/Thread/ThreadManager.hpp>
+
+namespace CppServerCore::Thread
+{
+    /*---------------------*
+     *    ThreadManager    *
+     *---------------------*/
+
+    ThreadManager::ThreadManager()
+    {
+        InitThreadLocalStorage();
+    }
+
+    ThreadManager::~ThreadManager()
+    {
+        JoinAll();
+    }
+
+    void ThreadManager::Launch(std::function<void()> callback)
+    {
+        auto lockGuard = std::lock_guard(_lock);
+
+        _threads.emplace_back([&callback]()
+                              {
+                                  InitThreadLocalStorage();
+                                  callback();
+                                  DestroyThreadLocalStorage();
+                              });
+    }
+
+    void ThreadManager::JoinAll()
+    {
+        for (std::thread& t : _threads)
+        {
+            if (t.joinable())
+            {
+                t.join();
+            }
+        }
+
+        _threads.clear();
+    }
+
+    void ThreadManager::InitThreadLocalStorage()
+    {
+        static std::atomic<uint32_t> s_threadId = static_cast<uint32_t>(ReservedThreadId::Main);
+        Common::t_threadId = s_threadId.fetch_add(1);
+    }
+
+    void ThreadManager::DestroyThreadLocalStorage()
+    {}
+}
---
+++ src/CppServerCore/Thread/ThreadManager.hpp
@@ -0,0 +1,37 @@
+﻿// CppServerCore/Thread/ThreadManager.hpp
+
+#pragma once
+
+namespace CppServerCore::Thread
+{
+    /*---------------------*
+     *    ThreadManager    *
+     *---------------------*/
+
+    class ThreadManager
+    {
+    private:
+        using           Mutex = std::mutex;
+        using           VecThread = std::vector<std::thread>;
+
+        enum class ReservedThreadId : uint32_t
+        {
+            Main = 1
+        };
+
+    public:
+                        ThreadManager();
+                        ~ThreadManager();
+
+        void            Launch(std::function<void()> callback);
+        void            JoinAll();
+
+    private:
+        static void     InitThreadLocalStorage();
+        static void     DestroyThreadLocalStorage();
+
+    private:
+        Mutex           _lock;
+        VecThread       _threads;
+    };
+}
